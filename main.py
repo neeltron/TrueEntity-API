c69898dbc92fc050ae8bf382ad8fdd8deb9540e6
neeltron <nprosofficial@gmail.com>
2023-09-23 13:09:27 +0000

    Fetching data from the app

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask('app')
+
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+
+@app.route('/input', methods=['POST'])
+def handle_post_request():
+  data = request.get_json()
+
+  name = data.get('name')
+  ssn = data.get('ssn')
+  passport = data.get('passport')
+  dob = data.get('dob')
+
+  print("hey", name, ssn, passport, dob)
+
+  response_data = {'message': 'Data received successfully'}
+  return jsonify(response_data), 200
+
 
 app.run(host='0.0.0.0', port=8080)
